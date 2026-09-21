62018d457cc142e48290b55a16054455fbccd57b
Avishek Prasad <avishekprasad0999@gmail.com>
2026-09-21 21:41:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function isSameType(value1, value2) {'
 	
-if (Number.isNaN(value1) && Number.isNaN(value2)) {
+if (Number.isNaN(value1) || Number.isNaN(value2)) {
 	return false;
 }
 	
 
-	if( typeof value1 ===  typeof value2){
+	else if( typeof value1 ===  typeof value2){
 		return true
 	}
 	else { return false)
